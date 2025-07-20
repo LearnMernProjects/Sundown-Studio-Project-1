2bab7ffb16b4b91f4a54d424b95f2f3650d04fd9
VirajNaik8055 <naikviraj301@gmail.com>
2025-07-20 13:55:53 +0530

    more changes to be done

---
+++ sscjs.js
@@ -0,0 +1,38 @@
+var elemC =document.querySelector("#elem-container")
+var fixed =document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter",function(){
+    fixed.style.display = "block";
+})
+elemC.addEventListener("mouseleave",function(){
+    fixed.style.display = "none";
+})
+var elem1 =document.querySelector("#elem")
+elem1.addEventListener("mouseenter",function(){
+    var image =elem1.getAttribute("data-image")
+    fixed.style.backgroundImage = `url(${image})`
+});
+var elems=document.querySelectorAll("#elem")
+elems.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var image =e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+
+})
+var images2=document.querySelector("#P")
+images2.addEventListener("onclick",function(){
+    var image21 =images2.getAttribute("data-image")
+    fixed.style.backgroundImage = `url(${image21})`
+})
+function swiperAnimation() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+}
+swiperAnimation();  
